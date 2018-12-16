ff1252136a900961d62b085b3d79b4246215fedf
OliverB <pilu@Pilus-MacBook-Pro.local>
2018-12-16 17:22:52 +0100

    Basic design FOS

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, page, page_meta, media, media_meta, component, component_meta, history;
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
@@ -6,5 +6,62 @@ CREATE TABLE users(
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE page (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    content TEXT,
+    status VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author INTEGER REFERENCES users(id),
+    languages VARCHAR(100)
+);
+
+CREATE TABLE page_meta (
+    pmeta_id SERIAL PRIMARY KEY,
+    page_id INTEGER REFERENCES page(id),
+    key VARCHAR(255),
+    value TEXT
+);
+
+CREATE TABLE media (
+    media_id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    url TEXT,
+    status VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE media_meta (
+    mmeta_id SERIAL PRIMARY KEY,
+    m_media_id INTEGER REFERENCES media(media_id),
+    key VARCHAR(255),
+    value TEXT
+);
+
+CREATE TABLE component (
+    component_id SERIAL PRIMARY KEY,
+    content TEXT,
+    status VARCHAR(100),
+    name VARCHAR(255),
+    author INTEGER REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE component_meta (
+    cmeta_id SERIAL PRIMARY KEY,
+    ccomponent_id INTEGER REFERENCES component(component_id),
+    key VARCHAR(255),
+    value TEXT
+);
+
+CREATE TABLE history (
+    history_id SERIAL PRIMARY KEY,
+    reference_id INTEGER,
+    content TEXT,
+    author INTEGER REFERENCES users(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- src/start.js
+++ src/start.js
@@ -4,14 +4,3 @@ import App from "./app";
 
 
 ReactDOM.render(<App />, document.querySelector("main"));
-
-// ReactDOM.render(
-//     <HelloWorld />,
-//     document.querySelector('main')
-// );
-//
-// function HelloWorld() {
-//     return (
-//         <div>Hello, World!</div>
-//     );
-// }

392b2581491ab41171841b82f7eec9f27901eb58
OliverB <pilu@Pilus-MacBook-Pro.local>
2019-01-09 10:09:46 +0100

    Fixed carousel

--- src/carousel.js
+++ src/carousel.js
@@ -10,37 +10,49 @@ export default class Carousel extends React.Component {
             <div className="jumbotron mt-5 mh-100">
 
 
-                <div id="aquarium" className="carousel slide" data-ride="carousel">
+                <div id="aquarium" className="carousel slide d-flex justify-content-center " data-ride="carousel">
                     <ul className="carousel-indicators">
                         <li data-target="#aquarium" data-slide-to="0" className="active"></li>
                         <li data-target="#aquarium" data-slide-to="1"></li>
                         <li data-target="#aquarium" data-slide-to="2"></li>
+                        <li data-target="#aquarium" data-slide-to="3"></li>
+                        <li data-target="#aquarium" data-slide-to="4"></li>
+                        <li data-target="#aquarium" data-slide-to="5"></li>
+                        <li data-target="#aquarium" data-slide-to="6"></li>
                     </ul>
 
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img src="https://s3.amazonaws.com/aquariumdesign/oYdLBAPAh6hLzc_8Ca9iiU96QC2J6q-f.PNG" alt="" />
-                            <div className="carousel-caption">
-                                <h3>Los Angeles</h3>
-                                <p>We had such a great time in LA!</p>
-                            </div>
+                            <img src="https://s3.amazonaws.com/aquariumdesign/dlJj-spNdlAgXXaNqBQ-T5JtMtDPrAQ7.jpg" alt="" />
+
                         </div>
 
                         <div className="carousel-item">
-                            <img src="https://s3.amazonaws.com/aquariumdesign/IFGqwTar0BAJyZJyn10PZnUoeS8BLXUt.jpg" alt="" />
-                            <div className="carousel-caption">
-                                <h3>Chicago</h3>
-                                <p>Thank you, Chicago!</p>
-                            </div>
+                            <img src="https://s3.amazonaws.com/aquariumdesign/dfAsm0n4pOwVeH3Vz19evScDv36IxAgT.jpg" alt="" />
+
                         </div>
 
                         <div className="carousel-item">
-                            <img src="https://s3.amazonaws.com/aquariumdesign/oYdLBAPAh6hLzc_8Ca9iiU96QC2J6q-f.PNG" alt="New York" />
-                            <div className="carousel-caption">
-                                <h3>New York</h3>
-                                <p>We love the Big Apple!</p>
-                            </div>
+                            <img src="https://s3.amazonaws.com/aquariumdesign/P6rfW-8iPbqksJbNpvGL9JqjAccrlxfB.jpg" alt="" />
                         </div>
+
+                        <div className="carousel-item">
+                            <img src="https://s3.amazonaws.com/aquariumdesign/Y1Er2HnACNivaLRAn3vsss0KdPOGIXz7.jpeg" alt="" />
+                        </div>
+
+                        <div className="carousel-item">
+                            <img src="https://s3.amazonaws.com/aquariumdesign/vbPO4duBxapr2kukErRsGNGGrYNJsL2x.jpg" alt="" />
+                        </div>
+
+                        <div className="carousel-item">
+                            <img src="https://s3.amazonaws.com/aquariumdesign/nMFMgrPC56gWgooz0CuYMeNCCQbRXXUu.jpg" alt="" />
+                        </div>
+
+                        <div className="carousel-item">
+                            <img src="https://s3.amazonaws.com/aquariumdesign/6jLrr7Xl8X7qlCT3708wM4ZlwITMrdAq.jpg" alt="" />
+                        </div>
+
+
                     </div>
                     <a className="carousel-control-prev" href="#aquarium" data-slide="prev">
                         <span className="carousel-control-prev-icon"></span>

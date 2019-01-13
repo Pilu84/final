6b535fe706c0cd3c4e845b7d7fda53382ceb577c
OliverB <pilu@Pilus-MacBook-Pro.local>
2019-01-13 13:35:36 +0100

    try without header

--- api/contact/index.php
+++ api/contact/index.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-$rest_json = file_get_contents("php://input");
-$_POST = json_decode($rest_json, true);
+
 if( empty($_POST['name']) && empty($_POST['email']) ) die();
 if ($_POST){
     // set response code - 200 OK
